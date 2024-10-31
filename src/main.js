c3f1babccf6f44f6e1ffeb4c708a4fcc56d6188d
watusi <lawwatusi@gmail.com>
2024-10-30 20:25:23 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createScene } from './scene.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('scene-container');
+    createScene(container);
+}); 
